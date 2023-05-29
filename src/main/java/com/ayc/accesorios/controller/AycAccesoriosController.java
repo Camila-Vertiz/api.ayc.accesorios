925b3d5eb016282af0a80a442022d7a06d89972f
Camila-Vertiz <camila.vrtz@gmail.com>
2023-05-29 17:13:10 -0500

    listar orden

--- src/main/java/com/ayc/accesorios/controller/AycAccesoriosController.java
+++ src/main/java/com/ayc/accesorios/controller/AycAccesoriosController.java
@@ -16,11 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ayc.accesorios.entity.Carrito;
 import com.ayc.accesorios.entity.Categoria;
 import com.ayc.accesorios.entity.DetalleOrden;
+import com.ayc.accesorios.entity.Orden;
 import com.ayc.accesorios.entity.Producto;
 import com.ayc.accesorios.entity.Usuario;
 import com.ayc.accesorios.service.ICarritoService;
 import com.ayc.accesorios.service.ICategoriaService;
 import com.ayc.accesorios.service.IDetalleOrdenService;
+import com.ayc.accesorios.service.IOrdenService;
 import com.ayc.accesorios.service.IProductoService;
 import com.ayc.accesorios.service.IUsuarioService;
 
@@ -41,6 +43,9 @@ public class AycAccesoriosController {
 	
 	@Autowired
 	private ICarritoService carritoService;
+	
+	@Autowired
+	private IOrdenService ordenService;
 	
 	@Autowired
 	private IDetalleOrdenService detalleService;
@@ -166,7 +171,24 @@ public class AycAccesoriosController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 	
+	@RequestMapping(value = "/orden/listar", method = RequestMethod.GET)
+	public ResponseEntity<List<Orden>> listarOrden() throws Exception {
+		List<Orden> listaOrden = ordenService.FindAll();
+		return ResponseEntity.ok(listaOrden);
+	}
 	
+	@RequestMapping(value = "/orden/insertar", method = RequestMethod.POST)
+	public ResponseEntity<?> insertarOrden(@RequestBody Orden orden) throws ParseException {
+		ordenService.Guardar(orden);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+	
+//	@RequestMapping(value="/orden/listar/{id_usuario}",method = RequestMethod.GET)
+//	public ResponseEntity<?> listarOrdenesPorUsuario(@PathVariable int id_usuario) throws Exception{
+//						
+//		List<Orden> data = ordenService.findById(id_usuario);
+//		return new ResponseEntity<>(data,HttpStatus.OK);
+//	}
 //	
 //	@RequestMapping(value = "/detalleOrden/listar", method = RequestMethod.GET)
 //	public ResponseEntity<List<DetalleOrden>> getDetalleOrden() throws Exception {
